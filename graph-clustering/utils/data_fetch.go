b55d7f01de911bb93a7d88d82e58dca55987b94f
xulikai <mmlymwhcs@live.com>
2024-03-17 00:09:06 +0800

    数据读取成流处理形式

---
+++ graph-clustering/utils/data_fetch.go
@@ -0,0 +1,96 @@
+package utils
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+const (
+	MAX_DATA_BUFFER_SIZE = 100
+)
+
+// RawData 无论是哪种格式的数据，都需要format成该标准格式后，再喂给聚类算法
+type RawData struct {
+	Title   string   `json:"title"`
+	DocNum  string   `json:"docno"`
+	SegList []string `json:"seg"`
+}
+
+type DataHandler struct {
+	filePath string
+	drainer  chan *RawData
+	err      chan error
+	done     chan struct{}
+}
+
+type Runner interface {
+	Start() error
+	Stop() error
+}
+
+func NewDataHandler(filePath string) *DataHandler {
+	return &DataHandler{
+		filePath: filePath,
+		drainer:  make(chan *RawData, MAX_DATA_BUFFER_SIZE),
+		err:      make(chan error),
+		done:     make(chan struct{}),
+	}
+}
+
+func (h *DataHandler) read(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		h.err <- errors.New("open file error: " + err.Error())
+		return
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+		}
+	}(file)
+
+	// 创建一个Scanner，用于逐行读取文件
+	scanner := bufio.NewScanner(file)
+
+	// 循环读取文件中的每一行
+	for scanner.Scan() {
+		// 获取当前行的文本内容
+		line := scanner.Text()
+
+		// 解析JSON字符串到结构体
+		var data RawData
+		err := json.Unmarshal([]byte(line), &data)
+		if err != nil {
+			fmt.Printf("Error parsing JSON: %v", err)
+			continue
+		}
+		h.drainer <- &data
+	}
+	h.done <- struct{}{}
+}
+
+// Start 可以传入一个function，来处理h.drainer通道的内容
+func (h *DataHandler) Start() error {
+	go h.read(h.filePath)
+	for {
+		select {
+		case data := <-h.drainer:
+			fmt.Printf("%v\n", data)
+		case err := <-h.err:
+			fmt.Printf("%v\n", err)
+			return err
+		case <-h.done:
+			return nil
+		}
+	}
+}
+
+func (h *DataHandler) Stop() error {
+	close(h.drainer)
+	close(h.err)
+	close(h.done)
+	return nil
+}
---
+++ graph-clustering/utils/data_fetch_test.go
@@ -0,0 +1,30 @@
+package utils
+
+import "testing"
+
+func TestDataHandler_Start(t *testing.T) {
+	type fields struct {
+		filePath string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "test01",
+			fields: fields{
+				filePath: "../../data/sougouCA_seg_simple.txt",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewDataHandler(tt.fields.filePath)
+			if err := h.Start(); (err != nil) != tt.wantErr {
+				t.Errorf("Start() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
